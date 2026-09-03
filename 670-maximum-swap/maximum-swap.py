688772319b48b922f82afa775fd4514d543f0b5e
abishek-9176 <abishek917614@gmail.com>
2026-09-03 21:04:48 +0530

    Time: 0 ms (100.00%) | Memory: 19.2 MB (64.62%) - LeetSync

---
+++ 670-maximum-swap/maximum-swap.py
@@ -0,0 +1,16 @@
+class Solution:
+    def maximumSwap(self, num: int) -> int:
+        k=list(map(int,str(num)))
+        v=sorted(k,reverse=True)
+        d=[]
+        if v==k:
+            return int(''.join(map(str, k)))
+        for i in range(len(k)):
+            if k[i]!=v[i]:
+                for j in range(i+1,len(k)):
+                    if k[j]==v[i]:
+                        d.append(j)
+                j=d[-1]
+                k[i],k[j]=k[j],k[i]
+                break
+        return int(''.join(map(str, k)))
